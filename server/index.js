dafecd86e59da8cfdadba2d08f6a7aad0af7befb
Juan Carlos Gonzalez <programmer1718@gmail.com>
2020-02-15 15:10:12 -0500

    basic implementation of public/access token exchange workflow

--- server/index.js
+++ server/index.js
@@ -12,7 +12,13 @@ app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// TODO: Create Plaid client. We will need it to make API calls to Plaid.
+var plaidClient = new plaid.Client(
+    plaidConfig.clientId,
+    plaidConfig.secret,
+    plaidConfig.publicKey,
+    plaid.environments[plaidConfig.env],
+    {version: '2019-05-29', clientApp: 'Personal Finance - API'}
+);
 
 app.get("/", function(req, res) {
     res.render("home.ejs", {
@@ -23,6 +29,26 @@ app.get("/", function(req, res) {
     })
 });
 
+app.post("/api/auth/get-access-token", async function(req, res) {
+    let { publicToken } = req.body;
+    
+    if (isNullOrEmpty(publicToken)) {
+        return res.status(400).json({ error: "A public token must be provided in the request body." });
+    }
+
+    try {
+        let tokenExchangeResponse = await plaidClient.exchangePublicToken(publicToken);
+        let { access_token, item_id } = tokenExchangeResponse;
+        // TODO: associate accessToken to itemId, and persist to a DB.
+        res.status(200).json({
+            accessToken: access_token,
+            itemId: item_id
+        });
+    } catch (tokenExchangeError) {
+        console.error({ tokenExchangeError });
+        res.status(500).json({ error: "An error occurred during the token exchange workflow." });
+    }
+});
 app.get("/home-exercise", function(req, res) {
     var accounts = [
         {
