54759d8dffcb914a74e3f601821bbdf9eef92997
Juan Carlos Gonzalez <programmer1718@gmail.com>
2020-02-15 09:36:57 -0500

    add template config file

---
+++ config/index-template.js
@@ -0,0 +1,12 @@
+var config = {
+    "plaid": {
+        "clientId": "",
+        "secret": "",
+        "publicKey": "",
+        "env": "sandbox",
+        "products": "transactions",
+        "countryCodes": "US,CA,GB,FR,ES"
+    }
+};
+
+module.exports = config;
