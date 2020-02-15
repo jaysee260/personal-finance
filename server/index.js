86a5856d31c0df9d92ec4a2b7ddc7f6c7fb2e545
Juan Carlos Gonzalez <programmer1718@gmail.com>
2020-02-15 10:59:14 -0500

    include plaid link client in home page

--- server/index.js
+++ server/index.js
@@ -3,10 +3,22 @@ var plaid = require("plaid");
 
 var app = express();
 var PORT = 3000;
+var { plaid:plaidConfig } = require("./config");
 
 app.set("view engine", "ejs");
 
+// TODO: Create Plaid client. We will need it to make API calls to Plaid.
+
 app.get("/", function(req, res) {
+    res.render("home.ejs", {
+        PLAID_PUBLIC_KEY: plaidConfig.publicKey,
+        PLAID_ENV: plaidConfig.env,
+        PLAID_PRODUCTS: plaidConfig.products,
+        PLAID_COUNTRY_CODES: plaidConfig.countryCodes
+    })
+});
+
+app.get("/home-exercise", function(req, res) {
     var accounts = [
         {
             id: 1,
@@ -26,7 +38,7 @@ app.get("/", function(req, res) {
         }
     ];
 
-    res.render("home.ejs", {
+    res.render("home-exercise.ejs", {
         accounts
     });
 });
