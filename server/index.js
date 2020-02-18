f3f1703469b4d2b8b3f78de7d3236ca044086581
Juan Carlos Gonzalez <programmer1718@gmail.com>
2020-02-17 20:46:15 -0500

    routing cleanup

--- server/index.js
+++ server/index.js
@@ -1,10 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
-var { plaid:plaidConfig } = require("./config");
-var plaidRouter = require("./routes/plaidRouter");
-
-
 var PORT = 3000;
 var app = express();
 
@@ -12,42 +8,13 @@ app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+
 // Register routes
+var landingRouter = require("./routes/landingRouter");
+var plaidRouter = require("./routes/plaidRouter");
+
+app.use("/", landingRouter);
 app.use("/api/plaid", plaidRouter);
-
-app.get("/", function(req, res) {
-    res.render("home.ejs", {
-        PLAID_PUBLIC_KEY: plaidConfig.publicKey,
-        PLAID_ENV: plaidConfig.env,
-        PLAID_PRODUCTS: plaidConfig.products,
-        PLAID_COUNTRY_CODES: plaidConfig.countryCodes
-    })
-});
-
-app.get("/home-exercise", function(req, res) {
-    var accounts = [
-        {
-            id: 1,
-            name: "Personal Checking"
-        },
-        {
-            id: 2,
-            name: "Joint Checking"
-        },
-        {
-            id: 3,
-            name: "Personal Savings"
-        },
-        {
-            id: 4,
-            name: "Joint Savings"
-        }
-    ];
-
-    res.render("home-exercise.ejs", {
-        accounts
-    });
-});
 
 app.listen(PORT, function() {
     console.log("Server running on port %s", PORT);
