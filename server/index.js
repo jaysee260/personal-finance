dbfb32dc326adb668a98b9d59776f16bb3a6290b
Juan Carlos Gonzalez <programmer1718@gmail.com>
2020-02-17 20:46:58 -0500

    install dotenv and mongoose

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 80;
 var app = express();
 
 app.set("view engine", "ejs");
