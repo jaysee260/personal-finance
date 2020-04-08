245ca6f97abcf9f951fd9ca5e80f3bc97d653189
Juan Carlos Gonzalez <programmer1718@gmail.com>
2020-04-08 15:28:36 -0400

    remove commented lines

--- server/src/controllers/index.js
+++ server/src/controllers/index.js
@@ -2,10 +2,6 @@ const landingController = require("./landingController");
 const authController = require("./authController");
 const plaidController = require("./plaidController");
 
-// module.exports.landingController = landingController;
-// module.exports.authController = authController;
-// module.exports.plaidController = plaidController;
-
 module.exports = {
   landingController,
   authController,
