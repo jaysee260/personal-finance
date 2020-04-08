1cca8c3a99a2ed262ce372c5e929dcabd5af6954
Juan Carlos Gonzalez <programmer1718@gmail.com>
2020-04-08 15:29:04 -0400

    this line got too long; split it into two lines

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 module.exports.initializeDb = function() {
   // Connect to Mongo DB
-  const mongoConnectionString = process.env.MONGO_DB_CONNECTION_STRING || require("../../config")[process.env.NODE_ENV].database.mongo.connectionString
+  const mongoConnectionString = 
+    process.env.MONGO_DB_CONNECTION_STRING ||
+    require("../../config")[process.env.NODE_ENV].database.mongo.connectionString;
+    
   mongoose.connect(
       mongoConnectionString,
       { useNewUrlParser: true, useUnifiedTopology: true },
